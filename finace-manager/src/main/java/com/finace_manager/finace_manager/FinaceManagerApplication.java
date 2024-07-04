25d314c3e5b0c08fd67d9f0dc6adcfa2d2d29b36
yablonovskydima <yablonovskydmutro1@gmail.com>
2024-07-04 19:50:49 +0300

    Init project

---
+++ finace-manager/src/main/java/com/finace_manager/finace_manager/FinaceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.finace_manager.finace_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinaceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinaceManagerApplication.class, args);
+	}
+
+}
---
+++ finace-manager/src/test/java/com/finace_manager/finace_manager/FinaceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finace_manager.finace_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinaceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
