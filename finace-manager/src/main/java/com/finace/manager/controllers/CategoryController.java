5dd149d0b4b33563bc59c5e58293c0edf546058b
yablonovskydima <yablonovskydmutro1@gmail.com>
2024-07-05 13:04:36 +0300

    Added controllers for enitties

--- finace-manager/src/main/java/com/finace/manager/controllers/CategoryController.java
+++ finace-manager/src/main/java/com/finace/manager/controllers/CategoryController.java
@@ -1,4 +1,56 @@
 package com.finace.manager.controllers;
 
-public class CategoryController {
+import com.finace.manager.entities.Category;
+import com.finace.manager.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController
+{
+    private final CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAll()
+    {
+        return ResponseEntity.ok(categoryService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable Long id)
+    {
+        return ResponseEntity.of(categoryService.getById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category, UriComponentsBuilder uriComponentsBuilder)
+    {
+        Category created = categoryService.create(category);
+        return ResponseEntity.created(uriComponentsBuilder.path("/categories/{id}")
+                .build(created.getId())).body(created);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateCategory(@RequestBody Category newCategory, @PathVariable Long id)
+    {
+        categoryService.update(id, newCategory);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteCategory(@PathVariable Long id)
+    {
+        categoryService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- finace-manager/src/main/java/com/finace/manager/controllers/TransactionController.java
+++ finace-manager/src/main/java/com/finace/manager/controllers/TransactionController.java
@@ -1,4 +1,56 @@
 package com.finace.manager.controllers;
 
-public class TransactionController {
+import com.finace.manager.entities.Transaction;
+import com.finace.manager.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/transactions")
+public class TransactionController
+{
+    private final TransactionService transactionService;
+
+    @Autowired
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Transaction>> getAll()
+    {
+        return ResponseEntity.ok(transactionService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Transaction> getTransactionById(@PathVariable Long id)
+    {
+        return ResponseEntity.of(transactionService.getById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Transaction> createTransactions(@RequestBody Transaction transaction, UriComponentsBuilder uriComponentsBuilder)
+    {
+        Transaction created = transactionService.create(transaction);
+        return ResponseEntity.created(uriComponentsBuilder.path("/transactions/{id}")
+                .build(created.getId())).body(created);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Transaction> updateTransaction(@RequestBody Transaction transaction, @PathVariable Long id)
+    {
+        transactionService.update(id, transaction);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Transaction> deleteTransaction(@PathVariable Long id)
+    {
+        transactionService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
