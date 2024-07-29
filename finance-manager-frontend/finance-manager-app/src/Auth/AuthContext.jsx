79e49b6a7d63dbc5096986a52a84664757f6e6d6
yablonovskydima <yablonovskydmutro1@gmail.com>
2024-07-30 02:06:33 +0300

    added refresh token logic, modified login form

--- finance-manager-frontend/finance-manager-app/src/Auth/AuthContext.jsx
+++ finance-manager-frontend/finance-manager-app/src/Auth/AuthContext.jsx
@@ -1,18 +1,23 @@
 import React, { createContext, useState, useEffect } from 'react';
 import axios from 'axios';
+import {jwtDecode} from 'jwt-decode';
 
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [username, setUsername] = useState('');
+  const [role, setRole] = useState('');
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:8083';
 
   useEffect(() => {
     const storedUsername = localStorage.getItem('username');
     const token = localStorage.getItem('accessToken');
+    const refreshToken = localStorage.getItem('refreshToken');
 
     if (storedUsername) {
       setUsername(storedUsername);
+      setIsAuthenticated(true);
     }
 
     const validateToken = async () => {
@@ -21,32 +26,63 @@ const AuthProvider = ({ children }) => {
           await axios.get(`${apiUrl}/api/v1/auth/validate`, {
             params: { token }
           });
-          setUsername(storedUsername);
+          const decodedToken = jwtDecode(token);
+          setRole(decodedToken.role);
         } catch (error) {
-          localStorage.removeItem('accessToken');
-          localStorage.removeItem('refreshToken');
-          localStorage.removeItem('username');
+          if (refreshToken) {
+            await refreshAccessToken(refreshToken);
+          } else {
+            logout();
+          }
         }
       }
     };
 
     validateToken();
-  }, [apiUrl]);
+  }, []);
 
-  const login = (username) => {
+  const refreshAccessToken = async (refreshToken) => {
+    try {
+      // Надсилаємо запит на оновлення токена
+      const response = await axios.post(`${apiUrl}/api/v1/auth/refresh`, { refreshToken });
+  
+      // Перевірка наявності поля `accessToken` у відповіді
+      if (response.data && response.data.accessToken) {
+        const { accessToken } = response.data;
+        localStorage.setItem('accessToken', accessToken);
+        const decodedToken = jwtDecode(accessToken);
+        setRole(decodedToken.role);
+        setIsAuthenticated(true);
+      } else {
+        throw new Error('Invalid response format');
+      }
+    } catch (error) {
+      console.error('Error refreshing access token:', error);
+      logout();
+    }
+  };
+
+  const login = (username, accessToken, refreshToken) => {
     setUsername(username);
     localStorage.setItem('username', username);
+    localStorage.setItem('accessToken', accessToken);
+    localStorage.setItem('refreshToken', refreshToken);
+    const decodedToken = jwtDecode(accessToken);
+    setRole(decodedToken.role);
+    setIsAuthenticated(true);
   };
 
   const logout = () => {
     setUsername('');
+    setRole('');
+    setIsAuthenticated(false);
     localStorage.removeItem('username');
     localStorage.removeItem('accessToken');
     localStorage.removeItem('refreshToken');
   };
 
   return (
-    <AuthContext.Provider value={{ username, login, logout }}>
+    <AuthContext.Provider value={{ username, role, isAuthenticated, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
